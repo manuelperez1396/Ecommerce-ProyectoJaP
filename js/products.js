b7af2d7db87bb2ac8fb609d683c7529a6670163f
manuelperez1396 <86933302+manuelperez1396@users.noreply.github.com>
2021-08-21 23:26:24 -0300

    Entrega 1
    Peticion de la lista de productos y login

--- js/products.js
+++ js/products.js
@@ -1,6 +1,49 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${product.imgSrc}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${product.name}</h4>
+                        <small class="text-muted">
+                         <b>${product.currency} $${product.cost} </b><br>
+                         ${product.soldCount}
+                        </small>
+                    </div>
+                    ${product.description}
+                    
+                </div>
+               
+            </div>
+        </div>
+        `
+
+        document.getElementById("ListaProductos").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e){
+
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+           productsArray = resultObj.data;
+            //Muestra los productos
+            showProductsList(productsArray);
+        }
+    });
 
 });
