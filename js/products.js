b75964fbc7818ef229c175eeccf52fa6bd784030
manuelperez1396 <86933302+manuelperez1396@users.noreply.github.com>
2021-09-05 18:24:51 -0300

    arregla error

--- js/products.js
+++ js/products.js
@@ -90,7 +90,6 @@ function sortAndShowProducts(sortCriteria, productsArray){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
