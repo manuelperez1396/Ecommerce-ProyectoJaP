753792a595d013b1c7b5a39ee94a87a43e0d8673
manuelperez1396 <86933302+manuelperez1396@users.noreply.github.com>
2021-09-16 12:59:33 -0300

    arreglo error y product info

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,94 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var currentProductsInfoArray = []; 
+var showProductsInfo =[];
 
+
+function showProductsInfo(){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsInfoArray.length; i++){
+        let info = currentProductsInfoArray[i];
+
+       
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+            <div class="row">
+            <div class="col-3">
+                <img src="${info.imgSrc}" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${info.name}</h4>
+                    <small class="text-muted">
+                     <b>${info.currency} $${info.cost} </b><br>
+                     ${info.soldCount}
+                    </small>
+                </div>
+                ${info.description}
+                
+            </div>
+            
+        </div>
+            `
+        }
+
+        document.getElementById("lista-productos").innerHTML = htmlContentToAppend;
+    }
+
+
+function showComments(array){
+    
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        var comment = array[i];
+    
+        htmlContentToAppend += `
+        <table style="width:100%">
+  
+            <tr>
+                <th><b>`+ comment.user +`</b></th>
+            </tr>
+            <tr>
+                <th>` + comment.score +`</th>
+            </tr>
+            <tr>
+                <td><i>`+ comment.description +`</i></td>
+            </tr>
+            <tr>
+                <td><small>`+ comment.dateTime +`</small></td>
+            </tr>
+        </table>
+        `
+    }
+        document.getElementById("comentarios").innerHTML = htmlContentToAppend;
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("NombreProducto");
+            let productDescriptionHTML = document.getElementById("descripcion");
+           
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            
+
+            //Muestra las imagenes de la galeria
+            
+            
+        }
+    });
+//Cargamos el JSON de los comentarios para poder mostrarlos
+getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+    if (resultObj.status === "ok") { comments = resultObj.data; }
+
+    //Se muestran los comentarios
+    showComments(comments);
+});
 });
